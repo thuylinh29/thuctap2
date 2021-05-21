b32dc17aac7770256983572149e3dee788ae2378
thuylinh29 <thuylinh29.gttn@gmail.com>
2021-05-21 13:03:32 +0700

    1

---
+++ frmDangNhap.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ThucTap2
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.lbTenDangNhap = new System.Windows.Forms.Label();
+            this.lbMatKhau = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(142, 12);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(168, 20);
+            this.txtTenDangNhap.TabIndex = 0;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(142, 51);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(168, 20);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // lbTenDangNhap
+            // 
+            this.lbTenDangNhap.AutoSize = true;
+            this.lbTenDangNhap.Location = new System.Drawing.Point(55, 15);
+            this.lbTenDangNhap.Name = "lbTenDangNhap";
+            this.lbTenDangNhap.Size = new System.Drawing.Size(81, 13);
+            this.lbTenDangNhap.TabIndex = 2;
+            this.lbTenDangNhap.Text = "Tên đăng nhập";
+            // 
+            // lbMatKhau
+            // 
+            this.lbMatKhau.AutoSize = true;
+            this.lbMatKhau.Location = new System.Drawing.Point(84, 54);
+            this.lbMatKhau.Name = "lbMatKhau";
+            this.lbMatKhau.Size = new System.Drawing.Size(52, 13);
+            this.lbMatKhau.TabIndex = 3;
+            this.lbMatKhau.Text = "Mật khẩu";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThoat.Location = new System.Drawing.Point(235, 90);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 18;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDangNhap.Location = new System.Drawing.Point(142, 90);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 19;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 136);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.lbMatKhau);
+            this.Controls.Add(this.lbTenDangNhap);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label lbTenDangNhap;
+        private System.Windows.Forms.Label lbMatKhau;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
+
---
+++ frmDangNhap.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ThucTap2
+{
+    public partial class frmDangNhap : Form
+    {
+        
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private string getId(string user, string pass)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-NALUFR0D\MSSQLSERVER01;Initial Catalog=Mixue;Integrated Security=True");
+            string id = "";
+            SqlCommand cmd = new SqlCommand("Select * from tblTaiKhoan where TenDangNhap = N'" + txtTenDangNhap.Text + "'" +
+                "and MatKhau=N'" + txtMatKhau.Text + "'", conn);
+            cmd.Connection = conn;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    id = dr["Id"].ToString();
+                }
+            }
+            return id;
+        }
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string sql="Select * from tblTaiKhoan where TenDangNhap=N'" + txtTenDangNhap.Text + "'" +
+                "and MatKhau=N'" + txtMatKhau.Text + "'";
+            string id_user;
+            id_user = getId(txtTenDangNhap.Text, txtMatKhau.Text);
+            if (id_user != "")
+            {
+                MessageBox.Show("Đăng nhập thành công");
+                this.Hide();
+                Form main = new frmPhieuNhapKho(); //sửa frmPhieuNhapKho thành form chính nhá
+                main.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Đăng nhập không thành công!");
+                txtTenDangNhap.Text = "";
+                txtMatKhau.Text = "";
+                txtTenDangNhap.Focus();
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát chương trình không", "Hỏi Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                this.Close();
+        }
+    }
+}
